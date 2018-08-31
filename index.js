2970fb5e0be10c240f5d3b1d061720824a4e01a4
Roman <rozario91208@gmail.com>
2018-08-31 19:48:03 +0300

    fs

--- index.js
+++ index.js
@@ -1,35 +1,7 @@
-var express = require('express');
+const fs = require('fs');
 
-var app = express();
+fs.appendFile('file.txt', 'asdasfsdacfdvssdhyvsvh', error=> {
+   if (error) throw error;
 
-app.set('view engine', 'ejs');
-
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+   console.log('DOne');
 });
-
-app.get('/about', function (req, res) {
-    res.sendFile(__dirname + '/about.html');
-});
-
-app.get('/news/:id', function (req, res) {
-
-    var obj = {
-      pagagrafs: [
-          'Первый',
-          'Второй',
-          'Третий',
-          '4',
-          'пятый 5',
-          6
-      ]
-    };
-    res.render('news', {
-        newsId: req.params.id,
-        obj: obj
-    });
-});
-
-app.listen(3000);
-
-
