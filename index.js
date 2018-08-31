ae2ae50ec20aa02ea262b87942b2e87596cf2273
Roman <rozario91208@gmail.com>
2018-08-31 15:25:52 +0300

    express routes

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    
+    res.send('SEND!!!! This is HOME');
+});
+
+app.get('/news', function (req, res) {
+    res.send(' This is NEWS');
+});
+
+app.get('/news/:id', function (req, res) {
+    res.send('NEWS ID -' + req.params.id);
 });
 
 app.listen(3000);
