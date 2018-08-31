7389f34e56a5f7c21895fe748a2731af068698b2
Roman <rozario91208@gmail.com>
2018-08-31 13:42:26 +0300

    stream

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-var http = require('http');
+var fs = require('fs');
 
-var server = http.createServer(function (req, response) {
-    console.log('URL страницы: ' + req.url);
-    response.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-    response.end('Hi, my new server');
+var myRedShort = fs.createReadStream(__dirname + '/article.txt', 'utf8');
+var myWriteShort = fs.createWriteStream(__dirname + '/news.txt');
+
+myRedShort.on('data', (chunk) => {
+    myWriteShort.write(chunk);
 });
 
-server.listen(3000, '127.0.0.1');
-console.log('Мы отслеживаем порт :3000');
 
